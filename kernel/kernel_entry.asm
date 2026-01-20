390bb74c37d0268866f26e2e14f3b72b337543b1
Ravol1 <matteotavolo26@gmail.com>
2026-01-20 02:58:32 +0100

    added multiboot2 support

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -2,21 +2,34 @@
 [extern main]
 [global _start]
 
-
 section .multiboot
-align 4
-    dd 0x1BADB002              ; magic number
-    dd 0x00000000              ; flags
-    dd -(0x1BADB002 + 0x00000000)  ; checksum
+align 8
+header_start:
+    dd 0xE85250D6                   ; magic
+    dd 0                            ; architecture (0 = i386)
+    dd header_start - header_end    ; total_length (header + checksum = 16 byte)
+    dd -(0xE85250D6 + 0 + (header_start - header_end))       ; checksum
+
+    align 8
+    dd 0    ; type (0 = end tag)
+    dd 0    ; flags
+    dd 8    ; size
+header_end:
 
 section .text
 _start:
     cli
     
-    call clear_screen
+    ;call clear_screen
+    
+    push ebx        ; Pointer to multiboot info
+    push eax        ; Magic number
     call main
-    jmp $
+    add esp, 8
 
+    halt:
+    hlt
+    jmp halt
 
 VMEM_BOTTOM equ 0xB8000
 VMEM_TOP equ VMEM_BOTTOM+(80*25*2)
